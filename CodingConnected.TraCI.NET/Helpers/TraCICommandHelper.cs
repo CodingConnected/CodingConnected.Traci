ff6effc17c5f25d4f180f8c4cc92bcc22c3a62cb
Florian Jasche <FlorianJa@users.noreply.github.com>
2018-07-31 13:10:20 +0200

    Requested changes for TraCICommandHelper.cs
    - moved ExecuteCommand to the top of the file
    - changed if else to switch case in ExecuteSetCommand method

--- CodingConnected.TraCI.NET/Helpers/TraCICommandHelper.cs
+++ CodingConnected.TraCI.NET/Helpers/TraCICommandHelper.cs
@@ -1,10 +1,81 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace CodingConnected.TraCI.NET.Helpers
 {
 	internal static class TraCICommandHelper
 	{
-		internal static TraCICommand GetCommand(string id, byte commandType, byte messageType)
+        internal static Tres ExecuteSetCommand<Tres, Tvalue>(TraCIClient client, string id, byte commandType, byte messageType, Tvalue value)
+        {
+            TraCICommand command = null;
+
+            switch (Type.GetTypeCode(typeof(Tvalue)))
+            {
+                case TypeCode.Int32:
+                    {
+                        int i = Convert.ToInt32(value);
+                        command = GetCommand(id, commandType, messageType, i);
+                        break;
+                    }
+                case TypeCode.Double:
+                    {
+                        double d = Convert.ToDouble(value);
+                        command = GetCommand(id, commandType, messageType, d);
+                        break;
+                    }
+                case TypeCode.String:
+                    {
+                        string s = value as string;
+                        command = GetCommand(id, commandType, messageType, s);
+                        break;
+                    }
+                default:
+                    {
+                        if (value is List<string>)
+                        {
+                            List<string> los = value as List<string>;
+                            command = GetCommand(id, commandType, messageType, los);
+                        }
+                        break;
+                    }
+            }
+
+            if (command != null)
+            {
+                var response = client.SendMessage(command);
+
+                try
+                {
+                    return (Tres)TraCIDataConverter.ExtractDataFromResponse(response, commandType, messageType);
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+            else
+            {
+                return default(Tres);
+            }
+        }
+
+        internal static T ExecuteGetCommand<T>(TraCIClient client, string id, byte commandType, byte messageType)
+        {
+            var command = GetCommand(id, commandType, messageType);
+            var response = client.SendMessage(command);
+
+            try
+            {
+
+                return (T)TraCIDataConverter.ExtractDataFromResponse(response, commandType, messageType);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        internal static TraCICommand GetCommand(string id, byte commandType, byte messageType)
 		{
 			var bytes = new List<byte> { messageType };
 			bytes.AddRange(TraCIDataConverter.GetTraCIBytesFromASCIIString(id));
@@ -15,23 +86,6 @@ namespace CodingConnected.TraCI.NET.Helpers
 			};
 			return command;
 		}
-
-		internal static T ExecuteGetCommand<T>(TraCIClient client, string id, byte commandType, byte messageType)
-		{
-			var command = GetCommand(id, commandType, messageType);
-			var response = client.SendMessage(command);
-
-			try
-			{
-
-				return (T)TraCIDataConverter.ExtractDataFromResponse(response, commandType, messageType);
-			}
-			catch
-			{
-				throw;
-			}
-		}
-
 
         internal static TraCICommand GetCommand(string id, byte commandType, byte messageType, List<string> values)
         {
@@ -97,50 +151,7 @@ namespace CodingConnected.TraCI.NET.Helpers
             return command;
         }
 
-        internal static Tres ExecuteSetCommand<Tres, Tvalue>(TraCIClient client, string id, byte commandType, byte messageType, Tvalue value)
-        {
-            TraCICommand command = null;
-
-            if (value is double)
-            {
-                double d = System.Convert.ToDouble(value);
-
-                command = GetCommand(id, commandType, messageType, d);
-            }
-            else if (value is string)
-            {
-                string s = value as string;
-                command = GetCommand(id, commandType, messageType, s);
-            }
-            else if (value is List<string>)
-            {
-                List<string> los = value as List<string>;
-                command = GetCommand(id, commandType, messageType, los);
-            }
-            else if(value is int)
-            {
-                int i = System.Convert.ToInt32(value);
-                command = GetCommand(id, commandType, messageType, i);
-            }
-
-            if (command != null)
-            {
-                var response = client.SendMessage(command);
-
-                try
-                {
-                    return (Tres)TraCIDataConverter.ExtractDataFromResponse(response, commandType, messageType);
-                }
-                catch
-                {
-                    throw;
-                }
-            }
-            else
-            {
-                return default(Tres);
-            }
-        }
+        
     }
 }
 
